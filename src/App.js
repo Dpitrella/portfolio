dbb7e67b4d10c4e3a9bf19725eb0efae8a7989ad
Dante Pitrella <dantepitrella1@yahoo.com>
2025-02-13 17:03:36 +0100

    meta

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 import Portfolio from './Pages/Portfolio/Portfolio'
 import Formation from './Pages/Formation/Formation'
 import Skills from './Pages/Skills/Skills'
@@ -9,19 +9,34 @@ import Moi from './Pages/Moi/Moi'
 import ScrollBasedNavigation from './Components/ScrollBasedNavigation';
 
 
+import previewImage from '../public/2025-02-13.png';
+
 function App() {
   return (
-    <div className="App">
-      <ScrollBasedNavigation>
-        <Routes>
-          <Route path="/" element={<Moi />} />
-          <Route path='/portfolio' element={<Portfolio />} />
-          <Route path='/skills' element={<Skills />} />
-          <Route path='/formation' element={<Formation />} />
-          <Route path='/contact' element={<Contact />} />
-        </Routes>
-      </ScrollBasedNavigation>
-    </div>
+    <HelmetProvider>
+      <div className="App">
+        <Helmet>
+          <title>Dante Pitrella Portfolio</title>
+          <meta name="description" content="Portfolio de Dante Pitrella - Développeur Web" />
+          <meta property="og:title" content="Dante Pitrella - Portfolio" />
+          <meta property="og:description" content="Portfolio de Dante Pitrella - Développeur Web créatif et passionné" />
+          <meta property="og:image" content={previewImage} />
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:title" content="Dante Pitrella - Portfolio" />
+          <meta property="twitter:description" content="Portfolio de Dante Pitrella - Développeur Web créatif et passionné" />
+          <meta property="twitter:image" content={previewImage} />
+        </Helmet>
+        <ScrollBasedNavigation>
+          <Routes>
+            <Route path="/" element={<Moi />} />
+            <Route path='/portfolio' element={<Portfolio />} />
+            <Route path='/skills' element={<Skills />} />
+            <Route path='/formation' element={<Formation />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </ScrollBasedNavigation>
+      </div>
+    </HelmetProvider>
   );
 }
 
